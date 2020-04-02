d3d1819c08e3cc3dc6ff3907b1968ab9feb1acb5
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-04-02 13:08:09 -0700

    GET /bookmarks worksyaaaaaay!

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const BookmarksService = require('./bookmarks-service')
 
 const app = express();
 
@@ -14,6 +15,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.get('/bookmarks', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    BookmarksService.getAllBookmarks(knexInstance)
+        .then(book => {
+            res.json(book)
+        })
+        .catch(next)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
